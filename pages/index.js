ecb521b156b4ad4045fc093709a744b470c449c2
saadkhaan <junk@techie.com>
2021-06-24 09:33:45 +0500

    meta title updated

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Cartalogix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
